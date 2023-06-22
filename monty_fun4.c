ae2a790ca5fb304b67dd07c04e87b87bf4a3d6ed
Yab8702 <yaleletdessalegn11@gmail.com>
2023-06-22 15:56:41 +0000

    update

--- monty_fun4.c
+++ monty_fun4.c
@@ -9,12 +9,12 @@
 
 void _pchar(stack_t **stack, unsigned int line_number)
 {
-	if (!stack || *stack == NULL)
+	if (!stack || !*stack)
 	{
 		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n < 0 || (*stack)->n > 127)
+	if (!isascii((*stack)->n))
 	{
 		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
